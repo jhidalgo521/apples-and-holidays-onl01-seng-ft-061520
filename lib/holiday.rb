53fe427116e6b02494dd1072171b123075ccd0f4
Jose Hidago <jhidalgo521@gmail.com>
2020-06-26 01:20:13 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -42,6 +42,8 @@ def add_supply_to_memorial_day(holiday_hash, supply)
   holiday_hash[:spring].each do |holiday, decor|
   
         decor<< "Grill"
+      
+      end
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
